8f46e6c247f8028e812a3d4452a4bcc777410d49
Kamil-J-Prokop <154501726+Kamil-J-Prokop@users.noreply.github.com>
2026-09-17 12:29:56 +0200

    Add: longest-repeating-substring-with-replacement - submission-6

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-6.py
@@ -0,0 +1,18 @@
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        l = 0  #left pointer
+        longest_substring = 0
+        counts = {}
+
+        for i, char in enumerate(s):
+            counts[char] = counts.get(char, 0) + 1
+
+            while ((i - l + 1) - max(counts.values())) > k:
+                counts[s[l]] -= 1
+                l += 1
+
+
+            longest_substring = max(longest_substring, i - l + 1)
+
+        return longest_substring
+
